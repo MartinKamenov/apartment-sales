8be786f0510fb2512cf984eb1e0b17059e97a46f
Martin Kamenov <martykam@abv.bg>
2018-03-03 14:52:02 +0200

    Removed unnecessary views. Removed comments. Started refactoring.

--- apartment-sales/models/PropertyRepository.js
+++ apartment-sales/models/PropertyRepository.js
@@ -1,30 +1,30 @@
-const collectionName = 'properties';
 class PropertyRepository {
-    constructor(database) {
+    constructor(database, collectionName) {
+        this.collectionName = collectionName;
         this.database = database;
     }
     getAllProperties() {
-        return this.database.showAll(collectionName);
+        return this.database.showAll(this.collectionName);
     }
 
     insertProperty(property) {
-        return this.database.insert(collectionName, property);
+        return this.database.insert(this.collectionName, property);
     }
 
     findPropertyByCode(code) {
-        return this.database.find(collectionName, { code: code });
+        return this.database.find(this.collectionName, { code: code });
     }
 
     updateProperty(code, property) {
-        this.database.delete(collectionName, { code: code }, property);
+        this.database.delete(this.collectionName, { code: code }, property);
     }
 
     findPropertyByParams(params) {
-        return this.database.find(collectionName, params);
+        return this.database.find(this.collectionName, params);
     }
 
     removeProperty(code) {
-        return this.database.delete(collectionName, { code: code });
+        return this.database.delete(this.collectionName, { code: code });
     }
 }
 
